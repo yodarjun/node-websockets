b71af7aff54a4aba4de89b9804b6b36ce97d91b9
Arjun Anand <aanand@ltvco.com>
2021-08-12 20:28:21 -0400

    .

--- server.js
+++ server.js
@@ -11,17 +11,33 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
-var count = 0;
+var total = 0;
+var individualCounts = {};
+var players = [];
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
   ws.on('message', (message) => {
+    console.log('message received: ' + message);
     if (message === "reset") {
-      count = 0;
+      total = 0;
+      players.forEach((key) => {
+        individualCounts[key] = 0;
+      });
+    } else if (message === "resetFull") {
+      total = 0;
+      individualCounts = {}
+      players = []
+      broadcast("register:" + JSON.stringify(players));
+    } else if (message.match(/register:/)) {
+      var tmpName = message.split(":")[1];
+      individualCounts[tmpName] = 0;
+      players.push(tmpName);
+      broadcast("register:" + JSON.stringify(players));
     } else {
-      count += 1;
-      console.log("count: " + count);
+      total += 1;
+      individualCounts[message] += 1;
     }
   });
 
@@ -29,7 +45,13 @@ wss.on('connection', (ws) => {
 });
 
 setInterval(() => {
+  var totalObj = { "total": total };
+  var msg = { ...individualCounts, ...totalObj };
+  broadcast(JSON.stringify(msg));
+}, 1000);
+
+function broadcast(msg) {
   wss.clients.forEach((client) => {
-    client.send(count);
+    client.send(msg);
   });
-}, 10);
+};
